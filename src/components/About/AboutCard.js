6ee4d21093c5b62c7d77d13b8af93e8a5a7a43e4
David_Liu <lzy625842840@163.com>
2024-01-12 05:09:42 +0800

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,7 +10,7 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             I am <span className="purple">David Liu </span>
             from <span className="purple"> China</span>
-            <br />I am a student from Chinese University of Hong Kong, Shenzhen
+            <br />I am a student from Chinese University of Hong Kong
             <br />
             <br />
             My game-related experiences have inspired me to pursue a career 
